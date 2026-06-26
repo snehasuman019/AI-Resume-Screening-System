18a59853b4fbe24768d0bb3a861fdea5e6ea40bf
Sneha Suman <snehasuman1119@gmail.com>
2026-06-26 10:55:46 +0530

    Integrated mongoDB and stored candidate analysis

--- Backend/app/routers/matching.py
+++ Backend/app/routers/matching.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-
+from datetime import datetime
 from app.models.job_description import JobDescriptionRequest
-
+from app.database.mongodb import candidate_collection
 from app.services.pdf_parser import extract_text_from_pdf
 from app.services.embedding_service import get_embedding
 from app.services.matching_service import calculate_similarity
@@ -51,10 +51,21 @@ def match(request: JobDescriptionRequest):
     skills_found, missing_skills = analyze_skill_gap(
         resume_text,
         request.job_description
-    )
-
-    return {
+        )
+    
+    candidate = {
+        "resume_filename": "CV Sneha.pdf",
+        "resume_text": resume_text,
+        "job_description": request.job_description,
         "match_score": round(score, 2),
         "skills_found": skills_found,
-        "missing_skills": missing_skills
+        "missing_skills": missing_skills,
+        "created_at": datetime.utcnow()
     }
+    result = candidate_collection.insert_one(candidate)
+    return {
+    "candidate_id": str(result.inserted_id),
+    "match_score": round(score, 2),
+    "skills_found": skills_found,
+    "missing_skills": missing_skills
+}
